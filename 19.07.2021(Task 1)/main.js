c14287f10afedae64823d9ac7a0dc68c24445f34
lexfor <thetim182001@mail.ru>
2021-07-22 11:55:02 +0300

    Lesson-1: delete new lines

--- 19.07.2021(Task 1)/main.js
+++ 19.07.2021(Task 1)/main.js
@@ -19,8 +19,6 @@ function setAllLevel(level){
     setStability(level);
 }
 
-
-
 function setABS(num) { console.log(`ABS:${num}`) }
 function setTraction(num) { console.log(`Traction Control:${num}`) }
 function setStability(num) { console.log(`Stability Control:${num}`) }
