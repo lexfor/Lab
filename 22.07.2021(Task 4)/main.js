2cc56d3670ad609bc5ba00fc0739bf41d7c72004
lexfor <thetim182001@mail.ru>
2021-07-26 14:15:25 +0300

    Lesson-2: fix setNums

--- 22.07.2021(Task 4)/main.js
+++ 22.07.2021(Task 4)/main.js
@@ -1,59 +1,78 @@
-function createSquare(N) {
+function createSquare(squareSize) {
     let resultArr = [];
-    let M = N * (N * N + 1) / 2;
-    const max = N * N;
+    let elemSumm = squareSize * (squareSize * squareSize + 1) / 2;
+    const max = squareSize * squareSize;
     const position = (max + 1) / 2;
     let nums = [];
     for(let i = 1;i <= max; i++){
         nums.push(i);
     }
-    for(let i = 0; i < N; i++){
+    for(let i = 0; i < squareSize; i++){
         resultArr.push([]);
-        for(let j = 0; j < N; j++){
+        for(let j = 0; j < squareSize; j++){
             resultArr[i].push(0);
         }
     }
-    if(N % 2 !== 0){
-        resultArr[ Math.floor(N / 2)][Math.floor(N / 2)] = position;
+    if(squareSize % 2 !== 0){
+        resultArr[ Math.floor(squareSize / 2)][Math.floor(squareSize / 2)] = position;
         nums.splice(nums.indexOf(position),1);
-        resultArr[0][0] = position + N;
-        nums.splice(nums.indexOf(position + N),1);
-        resultArr[0][N - 1] = position - 1;
+        resultArr[0][0] = position + squareSize;
+        nums.splice(nums.indexOf(position + squareSize),1);
+        resultArr[0][squareSize - 1] = position - 1;
         nums.splice(nums.indexOf(position - 1),1);
-        resultArr[N - 1][0] = position + 1;
+        resultArr[squareSize - 1][0] = position + 1;
         nums.splice(nums.indexOf(position + 1),1);
-        resultArr[N - 1][N - 1] = position - N;
-        nums.splice(nums.indexOf(position - N),1);
+        resultArr[squareSize - 1][squareSize - 1] = position - squareSize;
+        nums.splice(nums.indexOf(position - squareSize),1);
     }
-    setNums(resultArr,nums,M, N);
+    setNums(resultArr,nums,elemSumm, squareSize);
     return resultArr;
 }
 
-function setNums(array, nums, M, N) {
-    for(let i = 0; i < N; i++){
-        for(let j = 0; j < N; j++){
-            for(let k of nums){
+function setNums(array, leftNums, elemSumm, squareSize) {
+    if(checkSquare(array, elemSumm)){
+        return array;
+    }
+    for(let i = 0; i < squareSize; i++){
+        for(let j = 0; j < squareSize; j++){
+            for(let k of leftNums){
+                if(k === 0){
+                    continue;
+                }
                 if(array[i][j] === 0){
-                    let temp = [];
-                    Object.assign(temp,nums);
-                    let tempArr = [];
-                    Object.assign(tempArr,array);
-                    temp.splice(temp.indexOf(k),1);
-                    tempArr[i][j] = k;
-                    if(setNums(tempArr,temp,M,N)){
-                        console.log("1");
-                        array[i][j] = k;
-                        nums.splice(nums.indexOf(k),1);
+                    array[i][j] = k;
+                    let index = leftNums.indexOf(k);
+                    let value = k;
+                    leftNums[index] = 0;
+                    if(setNums(array,leftNums,elemSumm,squareSize)){
+                        return array;
                     }else{
-                        console.log(array);
+                        array[i][j] = 0;
+                        leftNums[index] = value;
                     }
                 }
             }
         }
-        if(nums.length === 0){
-            return array[i].reduce((sum, current) => sum + current, 0) === M;
+    }
+}
+
+function checkSquare(array, M) {
+    for(let i = 0;i < array.length; i++){
+        let res = array[i].reduce((sum,current) => sum + current,0);
+        if(res !== M){
+            return false;
         }
     }
+    for(let i = 0; i < array.size;i++){
+        let summ = 0;
+        for(let j = 0; j < array.size();j++){
+            summ += array[j][i];
+        }
+        if(summ !== M){
+            return false;
+        }
+    }
+    return true;
 }
 
 console.time("start");
