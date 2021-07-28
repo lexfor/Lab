7dd929a33e979ab77be403668c9707d89801e5b7
lexfor <thetim182001@mail.ru>
2021-07-28 19:20:18 +0300

    Lesson-3: added singleton pattern

---
+++ 26.07.2021(Additional)/StringFormatte.js
@@ -0,0 +1,24 @@
+/* eslint no-underscore-dangle: ["error", { "allow": ["_instance",] }] */
+
+export default class StringFormatte {
+  constructor(inputString) {
+    if (!StringFormatte._instance) {
+      StringFormatte._instance = this;
+    }
+    StringFormatte._instance.string = inputString;
+    return StringFormatte._instance;
+  }
+
+  removeNonUniqueChars() {
+    const symbols = [];
+    const stringArray = Array.from(this.string);
+    const outputArray = [];
+    stringArray.forEach((item) => {
+      if (symbols.indexOf(item) === -1) {
+        symbols.push(item);
+        outputArray.push(item);
+      }
+    });
+    this.string = outputArray.join('');
+  }
+}
