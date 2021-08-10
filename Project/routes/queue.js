01f56a8d5e3ce8f6d3b4de7b371264b75d83f20a
lexfor <thetim182001@mail.ru>
2021-08-10 20:02:30 +0300

    Lesson-5: added start script

--- Project/routes/queue.js
+++ Project/routes/queue.js
@@ -10,7 +10,7 @@ const router = express();
 const ajv = new Ajv();
 
 router.get('/', (req, res) => {
-  res.sendFile('./public/queue.html', { root: '../Project' });
+  res.sendFile('./public/queue.html', { root: './Project' });
 });
 
 router.post('/add-patient', (req, res, next) => {
